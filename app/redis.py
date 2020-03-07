162439158e5c8a5819350faa31dd3b1161de9301
Luis Chavarriaga <luischa123@gmail.com>
2020-03-07 12:18:26 -0500

    modify host values to allow connecting to docker-compose services
    in this commit, the host names are modified in the db clients,
    which allows them to connect to the docker-compose services on the
    network.

--- app/redis.py
+++ app/redis.py
@@ -1,7 +1,10 @@
 import redis
 
-r = redis.Redis()
+r = redis.Redis(host="redis", port=6379)
 
 def insert_temp(tmsp, tmp, rcd):
     r.set(rcd.timestamp(), "(temperature {}, timestamp {})".format(tmp, tmsp.timestamp()) )
     print(rcd)
+
+# función para parsear el valor de los records de temperatura. Se empleará en una
+# futura versión.
